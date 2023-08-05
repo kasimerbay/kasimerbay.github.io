232209beb7b9c2f52819c97cd4efb03e058409e6
Ahmet Kasım Erbay <ahmetkasim2@gmail.com>
2023-08-05 11:17:25 +0300

    audio added

--- counter/assets/js/script.js
+++ counter/assets/js/script.js
@@ -9,13 +9,15 @@ var myfunc = setInterval(function() {
 var now = new Date().getTime();
 var timeleft = countDownDate - now;
 
-    
 // Calculating the days, hours, minutes and seconds left
 var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
 var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-    
+
+// play the music
+document.getElementById('music').play();
+
 // Result is output to the specific element
 document.getElementById("day").innerHTML = days 
 document.getElementById("hour").innerHTML = hours 
